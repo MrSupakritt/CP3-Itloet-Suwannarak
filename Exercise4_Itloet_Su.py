da052437d83383b849450a8d61ab4bb3e85bc24c
MrSupakritt <99985641+MrSupakritt@users.noreply.github.com>
2023-07-24 17:58:58 +0700

    Exercise4
    create basic score program

---
+++ Exercise4_Itloet_Su.py
@@ -0,0 +1,10 @@
+#เก็บคะแนน
+found_eng = 85.45
+gen_bus = 80.85
+intro_com_sys = 79.5
+com_prog = 90.5
+print("--- Your Score ---")
+print(f"Foundation English : {found_eng}")
+print(f"General Business : {gen_bus}")
+print(f"Introduction to Computer Systems : {intro_com_sys}")
+print(f"Computer Programming : {com_prog}")
